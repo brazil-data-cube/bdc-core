a8f83775c029a61d45b7d60e5796caba144055e0
raphaelrpl <raphael.wcosta@gmail.com>
2019-07-04 11:10:03 -0300

    Add decorator 'require_model' to validate flask requests with JSON Schema. It also includes setup py and initial tests prototype

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+setup(
+    name='bdc_core',
+    version='0.1',
+    description='Brazilian Data Cube Core Package',
+    author='Admin',
+    author_email='admin@admin.com',
+    license="MIT",
+    packages=['bdc_core'],
+    url='https://github.com/brazil-data-cube/',
+)
---
+++ tests/bdc_core/test_decorators.py
@@ -0,0 +1,38 @@
+from bdc_core.decorators import require_model
+from flask import Flask, request
+from werkzeug.exceptions import BadRequest
+import json
+import unittest
+
+
+app = Flask('')
+app.config['TESTING'] = True
+
+
+schema = {
+    "type": "object",
+    "properties": {"coverage": {"type": "string"}},
+    "required": ["coverage"]
+}
+
+@app.route("/")
+@require_model(schema)
+def route_index():
+    return json.dumps(request.args)
+
+
+class TestRequireModel(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_invalid_schema(self):
+        resp = self.app.get('/')
+
+        self.assertEqual(400, resp.status_code)
+        self.assertIn("'coverage' is a required property", resp.data.decode('utf-8'))
+
+    def test_valid_schema(self):
+        resp = self.app.get('/?coverage=coverageExample')
+
+        self.assertEqual(200, resp.status_code)
+        self.assertIn(json.dumps({"coverage": "coverageExample"}), resp.data.decode('utf-8'))
