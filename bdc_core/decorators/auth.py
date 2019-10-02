ece2200c60ad20905ea46359a86082fd57412d23
Beto Noronha <beto_noronha@live.com>
2019-10-02 11:58:46 -0300

    fix decorator to decode client_token

--- bdc_core/decorators/auth.py
+++ bdc_core/decorators/auth.py
@@ -14,32 +14,40 @@ def get_token():
     except Exception:
         raise Forbidden('Token is required. Please login!')
 
+
 def validate_scope(scope_required, scope_token):
     if scope_required:
         service, function, actions = scope_required.split(':')
         if (service != scope_token['type'] and scope_token['type'] != '*') or \
             (function not in scope_token['name'] or scope_token['name'] != '*') or \
-            (actions not in scope_token['actions'] or scope_token['actions'] != '*'):
+            (actions not in scope_token['actions'] or '*' in scope_token['actions']):
             raise Unauthorized('Scope not allowed!')
 
-def jwt_required(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        if not os.environ.get('CLIENT_SECRET_KEY'):
-            raise HTTPException('Set CLIENT_SECRET_KEY in environment variable')
 
-        try:
-            token = get_token()
-            payload = jwt.decode(token, os.environ.get('CLIENT_SECRET_KEY'))
-            if payload.get('user_id') and payload.get('access_token'):
-                request.user_id = payload['user_id']
-                validate_scope(kwargs['scope'], payload['access'][0])
-                return func(*args, **kwargs)
-            else:
-                raise Forbidden('Incomplete token. Please login!')           
+def require_oauth_scopes(scope):
+    def jwt_required(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            if not os.environ.get('CLIENT_SECRET_KEY'):
+                raise HTTPException('Set CLIENT_SECRET_KEY in environment variable')
+            if not os.environ.get('CLIENT_AUDIENCE'):
+                raise HTTPException('Set CLIENT_AUDIENCE in environment variable')
 
-        except jwt.ExpiredSignatureError:
-            raise Forbidden('This token has expired. Please login!')
-        except jwt.InvalidTokenError:
-            raise Forbidden('Invalid token. Please login!')
-    return wrapper
+            try:
+                token = get_token()
+                payload = jwt.decode(token, os.environ.get('CLIENT_SECRET_KEY'), verify=True,
+                    algorithms=['HS512'], audience=os.environ.get('CLIENT_AUDIENCE'))
+
+                if payload.get('user_id'):
+                    request.user_id = payload['user_id']
+                    validate_scope(scope, payload['access'][0])
+                    return func(*args, **kwargs)
+                else:
+                    raise Forbidden('Incomplete token. Please login!')           
+
+            except jwt.ExpiredSignatureError:
+                raise Forbidden('This token has expired. Please login!')
+            except jwt.InvalidTokenError:
+                raise Forbidden('Invalid token. Please login!')
+        return wrapper
+    return jwt_required
