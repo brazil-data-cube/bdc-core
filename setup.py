e6697290386a3b99a0892c94f623debcf6384c5a
raphaelrpl <raphael.wcosta@gmail.com>
2019-10-10 14:45:46 -0300

    fix setup dependencies

--- setup.py
+++ setup.py
@@ -3,8 +3,22 @@
 from setuptools import find_packages, setup
 
 
+tests_require = [
+    'pytest>=5.0.0,<6.0.0',
+]
+
+
+extras_require = {
+    "docs": [
+        'bdc-readthedocs-theme @ git+git://github.com/brazil-data-cube/bdc-readthedocs-theme.git@test-pip#egg=bdc-readthedocs-theme',
+        'Sphinx>=2.1.2',
+    ],
+    "tests": tests_require
+}
+
+
 setup(
-    name='bdc_core',
+    name='bdc-core',
     version='0.3',
     description='Brazilian Data Cube Core Package',
     author='Admin',
@@ -13,13 +27,11 @@ setup(
     packages=find_packages(),
     url='https://github.com/brazil-data-cube/',
     install_requires=[
-        'Flask==1.0.3',
-        'flask-restplus==0.12.1',
+        'Flask>=1.0.3',
+        'flask-restplus>=0.12.1',
+        'Flask-JWT>=0.3.2',
         'jsonschema==3.0.1',
-        'pytest>=5.0.0,<6.0.0',
-        'Sphinx>=2.1.2',
-        'bdc-readthedocs-theme @ git+git://github.com/brazil-data-cube/bdc-readthedocs-theme.git@test-pip#egg=bdc-readthedocs-theme',
-        'Werkzeug>=0.14.1,<1.0.0'
-    ]
-
+    ],
+    extras_require=extras_require,
+    tests_require=tests_require
 )
