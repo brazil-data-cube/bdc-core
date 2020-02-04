f74256cb27fa7cb4ad5c5bc249d98874c06390aa
Beto Noronha <beto_noronha@live.com>
2020-02-04 11:08:46 -0300

    fix template - send email

--- bdc_core/email/config.py
+++ bdc_core/email/config.py
@@ -1,6 +1,6 @@
 import os
 
-BASE_PATH_TEMPLATES = os.getenv('BASE_PATH_TEMPLATES', 'templates')
+BASE_PATH_TEMPLATES = os.getenv('BASE_PATH_TEMPLATES', '{}/bdc_core/email/templates'.format(os.getcwd()))
 
 SMTP_PORT = os.getenv('SMTP_PORT', 587)
 SMTP_HOST = os.getenv('SMTP_HOST', 'smtp.domain.com')
